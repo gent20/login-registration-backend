7199df7b78c763b0990dcadfbd658c8099fbdbce
gent20 <gentkastratio@gmail.com>
2021-01-21 21:28:20 +0100

    User Registration( controller, service and request)

--- src/main/java/com/spring/project/appuser/AppUserService.java
+++ src/main/java/com/spring/project/appuser/AppUserService.java
@@ -1,14 +1,23 @@
 package com.spring.project.appuser;
 
+import lombok.AllArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
+@AllArgsConstructor
 public class AppUserService implements UserDetailsService {
+
+    private final static String MESSAGE =
+            "User with email $s not found";
+    private final AppUserRepository appUserRepository;
+
     @Override
-    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
-        return null;
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return appUserRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException(
+                        String.format(MESSAGE, email)));
     }
 }
---
+++ src/main/java/com/spring/project/registration/RegistrationService.java
@@ -0,0 +1,7 @@
+package com.spring.project.registration;
+
+public class RegistrationService {
+    public String register(RegistrationRequest request) {
+        return null;
+    }
+}
